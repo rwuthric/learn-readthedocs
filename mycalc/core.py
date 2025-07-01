98dfa84c96ec6130b553046acd4e28e0913a7a2c
Rolf Wuthrich <rolf.wuthrich@concordia.ca>
2025-07-01 11:58:12 -0400

    Add sub

--- mycalc/core.py
+++ mycalc/core.py
@@ -15,3 +15,21 @@ def add(a: float, b: float) -> float:
         Sum of a and b
     """
     return a + b
+
+def sub(a: float, b: float) -> float:
+    """
+    Subtract two numbers.
+
+    Parameters
+    ----------
+    a : float
+        The number to subtract from
+    b : float
+        The number to subtract
+
+    Returns
+    -------
+    float
+        The result of a - b
+    """
+    return a - b
